69d6d17b001d2aaee1653cd54a3cc642d69e28d7
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-06-29 18:06:15 +0900

    Complete task

--- src/redux/commentApi.js
+++ src/redux/commentApi.js
@@ -1,15 +1,40 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_ENDPOINT = '/comments';
-const BASE_URL = '';
+const API_ENDPOINT = "/comments";
+const BASE_URL = "https://6662aeeb62966e20ef097e3e.mockapi.io";
 
 export const commentApi = createApi({
-  reducerPath: 'comments',
+  reducerPath: "comments",
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
   }),
-  tagTypes: ['Comments'],
-  endpoints: (builder) => ({}),
+  tagTypes: ["Comments"],
+  endpoints: (builder) => ({
+    getComments: builder.query({
+      query: () => API_ENDPOINT,
+      providesTags: ["Comments"],
+    }),
+    addComment: builder.mutation({
+      query: (newComment) => ({
+        url: API_ENDPOINT,
+        method: "POST",
+        body: newComment,
+      }),
+      invalidatesTags: ["Comments"],
+    }),
+    updateCommentCount: builder.mutation({
+      query: ({ id, ...count }) => ({
+        url: `${API_ENDPOINT}/${id}`,
+        method: "PUT",
+        body: count,
+      }),
+      invalidatesTags: ["Comments"],
+    }),
+  }),
 });
 
-export const {} = commentApi;
+export const {
+  useGetCommentsQuery,
+  useAddCommentMutation,
+  useUpdateCommentCountMutation,
+} = commentApi;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,15 @@
-import { Comments, Hero, Loader, Section } from '../components';
+import { Comments, Hero, Loader, Section } from "../components";
+import { useGetCommentsQuery } from "../redux/commentApi";
 
 export const Home = () => {
+  const { isLoading } = useGetCommentsQuery();
   return (
     <>
+      {isLoading && <Loader />}
       <Section>
         <Hero
-          title='What people are saying.'
-          subtitle='Feedback from our customers.'
+          title="What people are saying."
+          subtitle="Feedback from our customers."
         />
         <Comments />
       </Section>
